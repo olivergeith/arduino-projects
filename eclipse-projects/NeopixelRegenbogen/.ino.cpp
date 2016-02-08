df2a90d4c6d7de799e68f65b2977e72fdee5ed6c
Oliver <oliver.geith@gmx.net>
2016-02-08 20:09:07 +0100

    eclipse-project Neopixel mit 2 Potis

---
+++ eclipse-projects/NeopixelRegenbogen/.ino.cpp
@@ -0,0 +1,15 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-02-08 20:00:37
+
+#include "Arduino.h"
+#include <Adafruit_NeoPixel.h>
+#include "U8glib.h"
+void setup() ;
+void loop() ;
+uint32_t Wheel(byte WheelPos) ;
+void fillLCD(void);
+
+#include "NeopixelRegenbogen.ino"
+
