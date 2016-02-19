9b9446c5da3dd6a1b28fd2fcfa5c18119ddd3542
Oliver <oliver.geith@gmx.net>
2016-02-19 22:00:55 +0100

    VUMeter

---
+++ eclipse-projects/NeopixelVUMeter/.ino.cpp
@@ -0,0 +1,13 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-02-19 22:00:35
+
+#include "Arduino.h"
+#include <Adafruit_NeoPixel.h>
+void setup() ;
+void loop() ;
+uint32_t Wheel(byte WheelPos) ;
+
+#include "NeopixelVUMeter.ino"
+
